ffc8425ca96a5526d01cdd14c6fe47e4440d50f7
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-01-17 16:44:40 -0500

    add utils module

---
+++ s3pathlib/utils.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+
+import typing
+
+
+def split_s3_uri(
+    s3_uri: str,
+) -> typing.Tuple[str, str]:
+    """
+    Split AWS S3 URI, returns bucket and key.
+
+    :param s3_uri: example, ``"s3://my-bucket/my-folder/data.json"``
+    """
+    parts = s3_uri.split("/")
+    bucket = parts[2]
+    key = "/".join(parts[3:])
+    return bucket, key
+
+
+def join_s3_uri(
+    bucket: str,
+    key: str,
+) -> str:
+    """
+    Join AWS S3 URI from bucket and key.
+
+    :param bucket: example, ``"my-bucket"``
+    :param key: example, ``"my-folder/data.json"`` or ``"my-folder/"``
+    """
+    return "s3://{}/{}".format(bucket, key)
+
+
+def smart_join_s3_key(
+    parts: typing.List[str],
+    is_dir: bool,
+) -> str:
+    """
+    Note, it assume that there's no such double slack in your path. It ensure
+    that there's only one consecutive "/" in the s3 key.
+
+    :param parts: list of s3 key path parts, could have "/"
+    :param is_dir: if True, the s3 key ends with "/". otherwise enforce no
+        tailing "/".
+
+    Example::
+
+        >>> smart_join_s3_key(parts=["/a/", "b/", "/c"], is_dir=True)
+        a/b/c/
+        >>> smart_join_s3_key(parts=["/a/", "b/", "/c"], is_dir=False)
+        a/b/c
+    """
+    new_parts = list()
+    for part in parts:
+        new_parts.extend([chunk for chunk in part.split("/") if chunk])
+    key = "/".join(new_parts)
+    if is_dir:
+        return key + "/"
+    else:
+        return key
+
+
+def make_s3_console_url(
+    bucket: str = None,
+    prefix: str = None,
+    s3_uri: str = None,
+) -> str:
+    """
+    Return an AWS Console url that you can use to open it in your browser.
+
+    :param bucket: example, ``"my-bucket"``
+    :param prefix: example, ``"my-folder/"``
+    :param s3_uri: example, ``"s3://my-bucket/my-folder/data.json"``
+
+    Example::
+
+        >>> make_s3_console_url(s3_uri="s3://my-bucket/my-folder/data.json")
+        https://s3.console.aws.amazon.com/s3/object/my-bucket?prefix=my-folder/data.json
+    """
+    if s3_uri is None:
+        if not ((bucket is not None) and (prefix is not None)):
+            raise ValueError
+    else:
+        if not ((bucket is None) and (prefix is None)):
+            raise ValueError
+        bucket, prefix = split_s3_uri(s3_uri)
+
+    if prefix.endswith("/"):
+        s3_type = "buckets"
+    else:
+        s3_type = "object"
+    return "https://s3.console.aws.amazon.com/s3/{s3_type}/{bucket}?prefix={prefix}".format(
+        s3_type=s3_type,
+        bucket=bucket,
+        prefix=prefix
+    )
+
+
+def ensure_s3_object(
+    s3_key_or_uri: str,
+) -> None:
+    """
+    Raise exception if the string is not in valid format for a AWS S3 object
+    """
+    if s3_key_or_uri.endswith("/"):
+        raise ValueError("'{}' doesn't represent s3 object!".format(s3_key_or_uri))
+
+
+def ensure_s3_dir(
+    s3_key_or_uri: str
+) -> None:
+    """
+    Raise exception if the string is not in valid format for a AWS S3 directory
+    """
+    if not s3_key_or_uri.endswith("/"):
+        raise ValueError("'{}' doesn't represent s3 dir!".format(s3_key_or_uri))
---
+++ tests/test_utils.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import os
+from s3pathlib import utils
+
+dir_here = os.path.dirname(os.path.abspath(__file__))
+dir_project_root = os.path.dirname(dir_here)
+
+
+def test_split_s3_uri():
+    s3_uri = "s3://my-bucket/my-prefix/my-file.zip"
+    bucket, key = utils.split_s3_uri(s3_uri)
+    assert bucket == "my-bucket"
+    assert key == "my-prefix/my-file.zip"
+
+
+def test_join_s3_uri():
+    bucket = "my-bucket"
+    key = "my-prefix/my-file.zip"
+    s3_uri = utils.join_s3_uri(bucket, key)
+    assert s3_uri == "s3://my-bucket/my-prefix/my-file.zip"
+
+
+def test_s3_key_smart_join():
+    assert utils.smart_join_s3_key(
+        parts=["/a/", "b/", "/c"],
+        is_dir=True,
+    ) == "a/b/c/"
+
+    assert utils.smart_join_s3_key(
+        parts=["/a/", "b/", "/c"],
+        is_dir=False,
+    ) == "a/b/c"
+
+
+def test_make_s3_console_url():
+    url = utils.make_s3_console_url("my-bucket", "my-file.zip")
+    assert "object" in url
+
+    url = utils.make_s3_console_url("my-bucket", "my-folder/")
+    assert "bucket" in url
+
+    url = utils.make_s3_console_url(s3_uri="s3://my-bucket/my-folder/data.json")
+    assert url == "https://s3.console.aws.amazon.com/s3/object/my-bucket?prefix=my-folder/data.json"
+
+    with pytest.raises(ValueError):
+        utils.make_s3_console_url(bucket="")
+
+    with pytest.raises(ValueError):
+        utils.make_s3_console_url(prefix="", s3_uri="")
+
+
+def test_ensure_s3_object():
+    utils.ensure_s3_object("path/to/key")
+    with pytest.raises(Exception):
+        utils.ensure_s3_object("path/to/dir/")
+
+
+def test_ensure_s3_dir():
+    utils.ensure_s3_dir("path/to/dir/")
+    with pytest.raises(Exception):
+        utils.ensure_s3_dir("path/to/key")
+
+
+
+if __name__ == "__main__":
+    import os
+
+    basename = os.path.basename(__file__)
+    pytest.main([basename, "-s", "--tb=native"])
