ed74f86dd8712555d10d95fff806cdb58366cba9
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-01-18 18:08:06 -0500

    use different boto session for different runtime

--- s3pathlib/tests/__init__.py
+++ s3pathlib/tests/__init__.py
@@ -1,8 +1,27 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
 import boto3
 
-boto_ses = boto3.session.Session()
+if "CI" in os.environ:
+    aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID_FOR_GITHUB_CI"]
+    aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY_FOR_GITHUB_CI"]
+    boto_ses = boto3.session.Session(
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+        region_name="us-east-1"
+    )
+    runtime = "ci"
+else:
+    boto_ses = boto3.session.Session()
+    runtime = "local"
+
 s3_client = boto_ses.client("s3")
-bucket = "aws-data-lab-sanhe-for-everything"
-prefix = "unittest/s3pathlib"
+bucket = "aws-data-lab-sanhe-for-opensource"
+prefix = "unittest/s3pathlib/{runtime}/{os}/py{major}{minor}/".format(
+    runtime=runtime,
+    os=sys.platform,
+    major=sys.version_info.major,
+    minor=sys.version_info.minor
+)
